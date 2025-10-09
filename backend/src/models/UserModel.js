561902f8a995db0639a4bd24ca790f5ee4ddafd7
Kim Isabelle <isabelle.kim2023@campus-eni.fr>
2025-10-09 15:42:58 +0200

    ajout script create db et modif sequelize dans userModel

--- backend/src/models/UserModel.js
+++ backend/src/models/UserModel.js
@@ -1,5 +1,5 @@
 const { DataTypes } = require("sequelize");
-const sequelize = require("../db");
+const sequelize = require("../config/Database");
 
 const User_model = sequelize.define("User", {
     user_id: {
